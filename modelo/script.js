5d4b5a6b6cb4b53fd9645e13923ae29b70f3bfb1
ingrid traspadini bazarelo barbosa <74667375+ingridtraspadini@users.noreply.github.com>
2021-01-07 09:28:41 -0300

    Update script.js

--- modelo/script.js
+++ modelo/script.js
@@ -1,22 +1,20 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-   
-   
-    msg.innerHTML = `agora são ${hora} horas.`
+    
 
     if (hora >= 05 && hora < 13) {
-         msg.innerHTML = 'BOM DIA!'
+         msg.innerHTML = "Agora são " + hora + " horas," + " Bom dia!"
         img.src = 'imagens/manha.jpg'
         document.body.style.background = '#CF9D6A'
     } else if (hora >= 13 && hora <18) {
-        msg.innerHTML ='BOA TARDE!'
+        msg.innerHTML = "Agora são " + hora + " horas," + " Boa Tarde!"
         img.src = 'imagens/tarde.jpg'
         document.body.style.background = '#BA4E06'
     } else {
-        msg.innerHTML = 'BOA NOITE!'
+        msg.innerHTML = "Agora são " + hora + " horas," + " Bom Noite!"
         img.src = 'imagens/noite.jpg'
         document.body.style.background = '#032C64'
     }
